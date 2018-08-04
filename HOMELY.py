e618393feedb3356adc90ba2202bd8fc8627d845
gnattishness <1620192+gnattishness@users.noreply.github.com>
2018-08-05 09:45:51 +1000

    Renamed included git config, small debugging.
    Changed mode on post-checkout so it should work.

--- HOMELY.py
+++ HOMELY.py
@@ -6,28 +6,32 @@ import os
 from homely.files import mkdir, symlink
 from homely.system import haveexecutable, execute
 
-HERE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+HERE = os.path.dirname(os.path.realpath(__file__))
+
 
 def main():
-    symlink('bashrc', '.bashrc')
-    symlink('bash_profile', '.bash_profile')
-    symlink('profile', '.profile')  # TODO maybe add lines instead?
-    symlink('vimrc', '.vimrc')
+    symlink("bashrc", ".bashrc")
+    symlink("bash_profile", ".bash_profile")
+    symlink("profile", ".profile")  # TODO maybe add lines instead?
+    symlink("vimrc", ".vimrc")
     # TODO check if taskwarrior is installed and only symlink if so
-    symlink('taskrc', '.taskrc')
+    symlink("taskrc", ".taskrc")
 
     # git common config:
     # TODO error if git not present
     # TODO into separate function
-    gitpath = os.path.join(HERE_DIR, 'git_common')
-    execute('git config --global include.path ' + gitpath)
+    gitpath = os.path.join(HERE, "common.gitconfig")
+    execute(["git", "config", "--global", "include.path", gitpath])
+    # NOTE this might overwrite if there's another include.path already present
     # TODO revert/cleanup somehow?
     # TODO check that path specification using ~/ works
 
-    mkdir('.git-hooks')
-    symlink('post-checkout', '.git-hooks/post-checkout')
+    mkdir(".git-hooks")
+    symlink("post-checkout", ".git-hooks/post-checkout")
 
     return 0
 
-if __name__ == '__main__':
-    sys.exit(main)
+
+if __name__ in ("__main__", "HOMELY"):
+    sys.exit(main())
